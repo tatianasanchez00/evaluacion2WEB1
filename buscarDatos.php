beb6b2b6d5b5b47d9e63f24572d1fbee12cc35f7
tatianasanchez00 <sglauratatiana@gmail.com>
2020-06-29 10:12:10 -0500

    se actualiza proyecto

--- buscarDatos.php
+++ buscarDatos.php
@@ -5,15 +5,10 @@ include('baseDatos.php');
 if(isset($_POST["buscar"])){
     $nombre = $_POST["nombreUsuario"];
 
-        //crear una consulta de sql //
         $consulta= "SELECT Nombre, Correo, Contraseña FROM `usuarios` WHERE Nombre='$nombre'";
 
-        //crear un objeto de la clase bdd
 
         $operacionEnBaseDatos = new BaseDatos();
-
-        //usar un metodo de la clase bdd para escribir en la bdd//
-
 
        $resultado=$operacionEnBaseDatos-> consultarEnBaseDatos($consulta);  
 
--- eliminarDatos.php
+++ eliminarDatos.php
@@ -8,15 +8,12 @@ if (isset ($_POST ["eliminar" ])) {
 $nombre=$_POST ["eliminarUsuario"];
 
 
-//crear una consulta de sql //
 
 $consulta= "DELETE FROM `usuarios` WHERE Nombre='$nombre'";
 
 
-//crear un objeto de la clase bdd
 $OperacionEnBaseDatos= new BaseDatos();
 
-//usar un metodo de la clase bdd para escribir en la bdd//
 $OperacionEnBaseDatos -> alterarBaseDatos ($consulta);
 
 
